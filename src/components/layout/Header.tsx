77b1af73b4cc718b37d9d786a8149830f9de447d
valletgood <valletgood@naver.com>
2025-11-03 22:59:00 +0900

    fix: Header 컴포넌트 import 수정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,7 +5,7 @@ import {
   NavigationMenuItem,
   NavigationMenuLink,
 } from '@/components/ui/navigation-menu';
-import { ThemeToggle } from '@/components/theme/theme-toggle';
+import { ThemeToggle } from '@/components/theme/ThemeToggle';
 import { useState } from 'react';
 import { cn } from '@/lib/utils';
 import { NAV_ITEMS, type NavItem } from '@/define/navigationMenu';
